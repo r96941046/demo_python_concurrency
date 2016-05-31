013f72d60effc7d441a22d63a6a26e9951d7b9e2
Hung Yu-Ting <r96941046@gmail.com>
2016-05-31 15:38:56 +0800

    Adjust number of vocabularies in demo

--- helpers/config.py
+++ helpers/config.py
@@ -1,5 +1,5 @@
 SPEECH_URL = 'https://api.voicerss.org/?'
 API_KEY = '3aa2b77a68474d7e8676eee7fa55de47'
-TEXT_FILE_NAME = 'vocabularytest.txt'
+TEXT_FILE_NAME = 'vocabulary_short.txt'
 DOWNLOAD_DIR_NAME = 'Files'
 DOWNLOAD_FILE_TYPE = '.mp3'
