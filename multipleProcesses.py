6345ce42c792d5f73cf3724c1b45584b6d5e2521
Hung Yu-Ting <r96941046@gmail.com>
2016-05-31 15:14:23 +0800

    Update multipleProcesses to use voice rss api

--- multipleProcesses.py
+++ multipleProcesses.py
@@ -1,21 +1,21 @@
 import os
 
+# This part enables using Pool.map and Pool.apply on
+# a function defined in a class
+import copy_reg
+import types
+
 from urllib import urlencode
 
 from multiprocessing.pool import Pool
 
+from helpers import config
 from helpers.parseArgs import parse_args
 from helpers.speechDownloader import SpeechDownloader
 from helpers.progress import Progress
 
 
 CWD = os.getcwd()
-
-
-# This part enables using Pool.map and Pool.apply on
-# a function defined in a class
-import copy_reg
-import types
 
 
 def _pickle_method(m):
@@ -29,19 +29,11 @@ copy_reg.pickle(types.MethodType, _pickle_method)
 
 class SpeechDownloadService(object):
 
-    SPEECH_URL = 'http://translate.google.com/translate_tts?'
-    TEXT_FILE_NAME = 'vocabulary.txt'
-    DOWNLOAD_DIR_NAME = 'Files'
-    DOWNLOAD_FILE_TYPE = '.mp3'
-    REQUEST_HEADER = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'
-    }
-
     def __init__(self, language, task_path):
         self._language = language
         self._task_path = task_path
-        self._text_file_path = self._task_path + self.TEXT_FILE_NAME
-        self._download_dir = self._task_path + self.DOWNLOAD_DIR_NAME
+        self._text_file_path = self._task_path + config.TEXT_FILE_NAME
+        self._download_dir = self._task_path + config.DOWNLOAD_DIR_NAME
 
     def setup_download_dir(self):
         download_dir = self._download_dir
@@ -72,14 +64,15 @@ class SpeechDownloadService(object):
             for text in f:
                 text = text.rstrip('\n')
                 encoded_args = urlencode({
-                    'tl': language,
-                    'q': text
+                    'hl': language,
+                    'src': text,
+                    'key': config.API_KEY
                 })
 
-                url = self.SPEECH_URL + encoded_args
-                download_path = os.path.join(download_dir, text + self.DOWNLOAD_FILE_TYPE)
+                url = config.SPEECH_URL + encoded_args
+                download_path = os.path.join(download_dir, text + config.DOWNLOAD_FILE_TYPE)
 
-                downloader = SpeechDownloader(url, self.REQUEST_HEADER, download_path)
+                downloader = SpeechDownloader(url, download_path)
                 # Pool().apply() blocks until the process is finished
                 # pool.apply(downloader.download)
                 pool.apply_async(downloader.download, callback=update_progress)
